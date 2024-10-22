8258a8889786a676e6139ddbcac8e571831ab942
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2024-10-22 13:00:00 +0200

    dev: herited table

--- SQL/create_referentials.sql
+++ SQL/create_referentials.sql
@@ -7,10 +7,27 @@ DROP schema if exists "ref" CASCADE;
 CREATE schema "ref";
 GRANT ALL PRIVILEGES ON SCHEMA "ref" TO diaspara_admin ;
 GRANT ALL PRIVILEGES ON SCHEMA public TO diaspara_read ;
+GRANT ALL PRIVILEGES ON SCHEMA refwgeel TO diaspara_admin ;
 GRANT CONNECT ON DATABASE "dbmig" TO diaspara_read;
 ALTER DATABASE "dbmig" OWNER TO diaspara_admin;
 
+CREATE SCHEMA refeel;
+ALTER SCHEMA refeel OWNER TO diaspara_admin;
+CREATE SCHEMA refnas;
+ALTER SCHEMA refnas OWNER TO diaspara_admin;
+CREATE SCHEMA refbast;
+ALTER SCHEMA refbast OWNER TO diaspara_admin;
+CREATE SCHEMA reftrutta;
+ALTER SCHEMA reftrutta OWNER TO diaspara_admin;
 -- edit pg_hba.conf on the server
+
+
+DROP TABLE IF EXISTS "ref".tr_species_spe;
+CREATE TABLE "ref".tr_species_spe (
+     spe_code CHARACTER VARYING(3) PRIMARY KEY,
+     spe_codeices CHARACTER VARYING(6) NOT NULL, -- TODO CHANGE WHEN ICES VOCAB
+     spe_description TEXT);
+INSERT INTO "ref".tr_species_spe VALUES ('eel','ele','mieux que le saumon');
 
 
 -- TODO search countries for Salmon US
@@ -23,9 +40,49 @@ CREATE TABLE ref.tr_country_cou (
 );
 -- how to inegrate with hierarchical level in salmodb ? Same for eel ?
 
-ALTER TABLE ref.tr_country_cou OWNER TO postgres;
-GRANT ALL ON SCHEMA ref diaspara_admin;
-CREATE ROLE diaspara_read
+ALTER TABLE ref.tr_country_cou OWNER TO diaspara_admin;
+
+
+-- Habitat types
+
+-- TODO search ICES for this
+CREATE TABLE ref.tr_habitattype_hty (
+    hty_code character varying(2) NOT NULL,
+    hty_description text
+);
+
+ALTER TABLE ref.tr_habitattype_hty OWNER TO diaspara_admin;
+
+-- 
+
+DROP TABLE IF EXISTS ref.tr_lifestage_lfs CASCADE;
+CREATE TABLE ref.tr_lifestage_lfs (
+    lfs_code character varying(5) PRIMARY KEY, 
+    lfs_name character varying(30) NOT NULL,
+    lfs_definition TEXT,
+    lfs_spe_code CHARACTER VARYING(3) NOT NULL, -- TODO CHANGE WHEN ICES FOUND
+    CONSTRAINT c_fk_lfs_spe_code FOREIGN KEY (lfs_spe_code) REFERENCES "ref".tr_species_spe(spe_code) 
+    ON UPDATE CASCADE ON DELETE NO ACTION,
+    CONSTRAINT c_uk_lfs_name UNIQUE (lfs_name)
+);
+
+ALTER TABLE ref.tr_lifestage_lfs OWNER TO diaspara_admin;
+
+
+
+-- referential per species
+
+
+
+DROP TABLE IF EXISTS refeel.tr_lifestageeel_lfe;
+CREATE TABLE refeel.tr_lifestageeel_lfe()INHERITS("ref".tr_lifestage_lfs);
+INSERT INTO refeel.tr_lifestageeel_lfe SELECT *, 'eel' FROM refwgeel.tr_lifestage_lfs;
+
+SELECT * FROM  refeel.tr_lifestageeel_lfe;
+SELECT * FROM  ref.tr_lifestage_lfs;
+
+
+
 
 CREATE TABLE "ref".tr_pararmeter_parm (
   parm_id serial4 NOT NULL,
--- SQL/foreign_data_wrapper.sql
+++ SQL/foreign_data_wrapper.sql
@@ -6,7 +6,7 @@ CREATE SERVER wgeel_data_wrapper
   FOREIGN DATA WRAPPER postgres_fdw
   OPTIONS (host 'localhost', port '5432', dbname 'wgeel');
   
--- adapter avec user password pour geola
+-- 
 CREATE USER MAPPING FOR USER
   SERVER wgeel_data_wrapper
   OPTIONS (user 'postgres', password 'postgres');
@@ -17,17 +17,4 @@ IMPORT FOREIGN SCHEMA ref
     FROM SERVER wgeel_data_wrapper
     INTO refwgeel;
     
--- JOIN WITH limit lm_id 622
--- test is 2154
-
--- TODO dans R select randomly.
---  TODO recuperer point central idsegment pour creer geom point des pseudo absences
-
-   
--- création table segments_distance (dans public)
-  
--- copie de la table de l'autre serveur
-DROP TABLE IF EXISTS montepomi.t_limite_lm;
-CREATE TABLE montepomi.t_limite_lm AS SELECT * FROM geol.t_limite_lm; --940--1005 22/11
-CREATE INDEX ON montepomi.t_limite_lm USING gist(geom); 
-CREATE INDEX ON montepomi.t_limite_lm USING btree(lm_id);   
+GRANT ALL PRIVILEGES ON SCHEMA refwgeel TO diaspara_admin;
