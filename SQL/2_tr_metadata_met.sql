320c0701c81407f0462df7bdb7bb9039c90f5a28
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2025-01-19 20:46:09 +0100

    dev : remove reference to stockey in version #8

--- SQL/2_tr_metadata_met.sql
+++ SQL/2_tr_metadata_met.sql
@@ -53,14 +53,16 @@ GRANT SELECT ON ref.tr_nimble_nim TO diaspara_read;
 -- variables, so I'm moving this from the main table and adding to metadata
 -- some variables might get deprecated in time. 
 -- Unless they get a new version this might not change
+-- I have removed reference to stockkey as there are several stock keys
+-- for the work of WGNAS
 DROP TABLE IF EXISTS ref.tr_version_ver CASCADE;
 CREATE TABLE ref.tr_version_ver(
 ver_code TEXT PRIMARY KEY,
 ver_year INTEGER NOT NULL,
 ver_spe_code CHARACTER VARYING(3),
-ver_stockkey INTEGER NOT NULL,
-ver_stockkeylabel TEXT NOT NULL,
-ver_stockadvicedoi TEXT NOT NULL,
+--ver_stockkey INTEGER NOT NULL, 
+--ver_stockkeylabel TEXT NOT NULL,
+---ver_stockadvicedoi TEXT NOT NULL,
 ver_datacalldoi TEXT NULL,
 ver_version INTEGER NOT NULL,
 ver_description TEXT,
@@ -76,13 +78,13 @@ COMMENT ON COLUMN ref.tr_version_ver.ver_year
 IS 'Year of assessement.';
 COMMENT ON COLUMN ref.tr_version_ver.ver_spe_code 
 IS 'Species code e.g. ''ele'' references tr_species_spe.';
-COMMENT ON COLUMN ref.tr_version_ver.ver_stockkey 
-IS 'Stockkey (integer) from the stock database.';
-COMMENT ON COLUMN ref.tr_version_ver.ver_stockkeylabel 
-IS 'Ver_stockkeylabel e.g. ele.2737.nea.';
-COMMENT ON COLUMN ref.tr_version_ver.ver_stockadvicedoi 
-IS 'Advice DOI corresponding to column adviceDOI 
-when using icesASD::getAdviceViewRecord().';
+--COMMENT ON COLUMN ref.tr_version_ver.ver_stockkey 
+--IS 'Stockkey (integer) from the stock database.';
+--COMMENT ON COLUMN ref.tr_version_ver.ver_stockkeylabel 
+--IS 'Ver_stockkeylabel e.g. ele.2737.nea.';
+--COMMENT ON COLUMN ref.tr_version_ver.ver_stockadvicedoi 
+--IS 'Advice DOI corresponding to column adviceDOI 
+--when using icesASD::getAdviceViewRecord().';
 COMMENT ON COLUMN ref.tr_version_ver.ver_datacalldoi 
 IS 'Data call DOI, find a way to retreive that information 
 and update this comment';
