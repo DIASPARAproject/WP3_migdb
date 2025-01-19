07403ee7bdff02b26476abbdf9a2275b1b2ec6c1
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2025-01-19 20:15:01 +0100

    creating refnas content

---
+++ SQL/3_refnas.sql
@@ -0,0 +1,119 @@
+
+
+DROP TABLE IF EXISTS refnas.tr_metadata_met;
+
+CREATE TABLE refnas.tr_metadata_met()
+INHERITS (ref.tr_metadata_met);
+
+
+-- ADDING CONSTRAINTS
+
+ALTER TABLE refnas.tr_metadata_met ADD
+ CONSTRAINT t_metadata_met_pkey PRIMARY KEY(met_var, met_spe_code);
+ 
+ALTER TABLE refnas.tr_metadata_met ADD 
+  CONSTRAINT fk_met_spe_code FOREIGN KEY (met_spe_code)
+  REFERENCES ref.tr_species_spe(spe_code) 
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ ALTER TABLE refnas.tr_metadata_met ADD
+    CONSTRAINT ck_met_spe_code CHECK (met_spe_code='SAL'); 
+
+ ALTER TABLE refnas.tr_metadata_met ADD
+    CONSTRAINT fk_met_wkg_code FOREIGN KEY (met_wkg_code)
+  REFERENCES ref.tr_icworkinggroup_wkg(wkg_code) 
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE refnas.tr_metadata_met ADD
+    CONSTRAINT ck_met_wkg_code CHECK (met_wkg_code='WGNAS');
+
+
+ALTER TABLE refnas.tr_metadata_met ADD
+  CONSTRAINT fk_met_ver_code FOREIGN KEY (met_ver_code)
+  REFERENCES ref.tr_version_ver(ver_code) 
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE refnas.tr_metadata_met ADD
+  CONSTRAINT fk_met_oty_code FOREIGN KEY (met_oty_code) 
+  REFERENCES ref.tr_objecttype_oty (oty_code) ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE refnas.tr_metadata_met ADD
+  CONSTRAINT fk_met_nim_code FOREIGN KEY (met_nim_code) 
+  REFERENCES ref.tr_nimble_nim (nim_code) ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE refnas.tr_metadata_met ADD  
+  CONSTRAINT fk_met_mtr_code FOREIGN KEY (met_mtr_code)
+  REFERENCES ref.tr_metric_mtr(mtr_code)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE refnas.tr_metadata_met ADD
+  CONSTRAINT fk_met_uni_code FOREIGN KEY (met_uni_code)
+  REFERENCES ref.tr_units_uni(uni_code)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE refnas.tr_metadata_met ADD
+  CONSTRAINT fk_met_cat_code FOREIGN KEY (met_cat_code)
+  REFERENCES ref.tr_category_cat(cat_code)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE refnas.tr_metadata_met ADD
+  CONSTRAINT fk_met_oco_code FOREIGN KEY (met_oco_code)
+  REFERENCES ref.tr_outcome_oco(oco_code)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+
+--  COMMENTS FOR WGNAS
+
+
+COMMENT ON TABLE refnas.tr_metadata_met IS 
+'Table (metadata) of each variable (parameter) in the wgnas database.';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_var 
+IS 'Variable code, primary key on both met_spe_code and met_var.';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_spe_code 
+IS 'Species, SAL primary key on both met_spe_code and met_var.';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_ver_code 
+IS 'Code on the version of the model, see table tr_version_ver.';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_oty_code 
+IS 'Object type, single_value, vector, matrix see table tr_objecttype_oty.';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_nim_code 
+IS 'Nimble type, one of data, constant, output, other.';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_dim 
+IS 'Dimension of the Nimble variable, use {10, 100, 100} 
+to insert the description of an array(10,100,100).';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_dimname 
+IS 'Dimension of the variable in Nimble, use {''year'', ''stage'', ''area''}.';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_modelstage 
+IS 'Currently one of fit, other, First year.';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_type 
+IS 'Type of data in the variable, homewatercatches, InitialISation first year,
+abundance ....';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_location 
+IS 'Describe process at sea, e.g. Btw. FAR - GLD fisheries, or Aft. Gld fISheries.';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_fishery 
+IS 'Description of the fishery.';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_oco_code 
+IS 'Outcome of the fish, e.g. Released (alive), Seal damage,
+Removed (from the environment), refnaserences table tr_outcome_oco.';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_uni_code 
+IS 'Unit, refnaserences table tr_unit_uni.';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_cat_code 
+IS 'Broad category of data or parameter, 
+catch, effort, biomass, mortality, count ...refnaserences table tr_category_cat.';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_mtr_code 
+IS 'Code of the metric, refnaserences tr_metric_mtr, Estimate, Bound, SD, CV ....';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_definition 
+IS 'Definition of the metric.';
+COMMENT ON COLUMN refnas.tr_metadata_met.met_deprecated
+IS'Is the variable still used ?';
+
+GRANT ALL ON refnas.tr_metadata_met TO diaspara_admin;
+GRANT SELECT ON refnas.tr_metadata_met TO diaspara_read;
