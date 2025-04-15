0d7a1af0db808eb8cb7a014d084bef48b2ab0f4d
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2025-04-15 12:50:08 +0200

    dev : tr_habitattype

--- SQL/2_ref_tr_habitattype_hty.sql
+++ SQL/2_ref_tr_habitattype_hty.sql
@@ -3,8 +3,8 @@
 -- This mostly only imports some of the codes from the WLTYP vocab
 
 
-DROP TABLE IF EXISTS ref.tr_habitatype_hty CASCADE;
-CREATE TABLE ref.tr_habitatype_hty (
+DROP TABLE IF EXISTS ref.tr_habitattype_hty CASCADE;
+CREATE TABLE ref.tr_habitattype_hty (
   hty_id SERIAL PRIMARY KEY,
   hty_code TEXT NOT NULL UNIQUE,
   hty_description TEXT,
@@ -13,15 +13,15 @@ CREATE TABLE ref.tr_habitatype_hty (
   hty_icestablesource text
 );
 
-COMMENT ON TABLE ref.tr_habitatype_hty IS 'Table of habitat types, takes from the WLTYP vocab';
-COMMENT ON COLUMN ref.tr_habitatype_hty.hty_id IS 'Integer, primary key of the table';
-COMMENT ON COLUMN ref.tr_habitatype_hty.hty_code IS 'The code of the habitat';
-COMMENT ON COLUMN ref.tr_habitatype_hty.hty_description IS 'Definition of the lifestage';
-COMMENT ON COLUMN ref.tr_habitatype_hty.hty_icesvalue IS 'Code for the lifestage in the ICES database';
-COMMENT ON COLUMN ref.tr_habitatype_hty.hty_icesguid IS 'GUID in the ICES database';
-COMMENT ON COLUMN ref.tr_habitatype_hty.hty_icestablesource IS 'Source table in ICES vocab';
+COMMENT ON TABLE ref.tr_habitattype_hty IS 'Table of habitat types, takes from the WLTYP vocab';
+COMMENT ON COLUMN ref.tr_habitattype_hty.hty_id IS 'Integer, primary key of the table';
+COMMENT ON COLUMN ref.tr_habitattype_hty.hty_code IS 'The code of the habitat';
+COMMENT ON COLUMN ref.tr_habitattype_hty.hty_description IS 'Definition of the lifestage';
+COMMENT ON COLUMN ref.tr_habitattype_hty.hty_icesvalue IS 'Code for the lifestage in the ICES database';
+COMMENT ON COLUMN ref.tr_habitattype_hty.hty_icesguid IS 'GUID in the ICES database';
+COMMENT ON COLUMN ref.tr_habitattype_hty.hty_icestablesource IS 'Source table in ICES vocab';
 
 
-GRANT ALL ON ref.tr_habitatype_hty TO diaspara_admin;
-GRANT SELECT ON ref.tr_habitatype_hty TO diaspara_read;
+GRANT ALL ON ref.tr_habitattype_hty TO diaspara_admin;
+GRANT SELECT ON ref.tr_habitattype_hty TO diaspara_read;
 
