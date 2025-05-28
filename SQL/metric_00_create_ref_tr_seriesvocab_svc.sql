43bd50acce1ffc56e5b6cff362cef09f58c63bb1
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2025-05-28 08:19:05 +0200

    dev: series_vocab OK #22 #23

--- SQL/metric_00_create_ref_tr_seriesvocab_svc.sql
+++ SQL/metric_00_create_ref_tr_seriesvocab_svc.sql
@@ -3,13 +3,13 @@
 -- Here it identifies the upper level of a multitannual data collection
 -- procedure. One svc_id might be common to several working group.
 
-
+DROP TABLE IF EXISTS ref.tr_seriesvocab_svc  CASCADE;
 CREATE TABLE ref.tr_seriesvocab_svc (
   svc_id uuid,
   CONSTRAINT tr_seriesvocab_svc_pkey PRIMARY KEY (svc_id),
   svc_code text NOT NULL,
-  CONSTRAINT uk_svc_code UNIQUE,
-  svc_description NULL) 
+  CONSTRAINT uk_svc_code UNIQUE (svc_code),
+  svc_description TEXT NULL); 
 
 
 
@@ -18,7 +18,7 @@ COMMENT ON TABLE ref.tr_seriesvocab_svc IS 'Table of time series vocabulary, or 
 It can correspond to time series data or individual metrics collection or both.';
 COMMENT ON COLUMN ref.tr_seriesvocab_svc.svc_id IS 'UUID, identifier of the series';
 COMMENT ON COLUMN ref.tr_seriesvocab_svc.svc_code IS 'Code of the series unique';
-
+COMMENT ON COLUMN ref.tr_seriesvocab_svc.svc_code IS 'Description of the series';
 
 GRANT ALL ON ref.tr_seriesvocab_svc TO diaspara_admin;
 GRANT SELECT ON ref.tr_seriesvocab_svc TO diaspara_read; 
