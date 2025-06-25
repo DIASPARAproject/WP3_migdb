4b17fa40cacfe5f828eb2d23522fd707739a3051
Jani Helminen <54444266+Nojani@users.noreply.github.com>
2025-06-25 20:31:22 +0300

    monitoring and fishway updates

--- SQL/metric_04_ref_tr_fishway_fiw.sql
+++ SQL/metric_04_ref_tr_fishway_fiw.sql
@@ -16,43 +16,43 @@ VALUES('VS',
 INSERT INTO ref.tr_fishway_fiw (fiw_code, fiw_description, fiw_definition)
 VALUES('PO', 
 'Pool', 
-'TODO');
+'A pool pass consists of a stepped channel divided by cross-walls that form a series of pools, where water flows through submerged or surface openings, allowing fish to rest between short bursts through higher-velocity zones. The rough bottom and calm pools make it especially suitable for both swimming and bottom-dwelling species.');
 INSERT INTO ref.tr_fishway_fiw (fiw_code, fiw_description, fiw_definition)
 VALUES('FL', 
 'Fish lock', 
-'TODO');
+'A fish lock, similar in structure to a ship lock, uses a lock chamber with inlet and outlet gates to help fish ascend past barriers; unlike ship locks, it is specifically designed to support fish migration by addressing issues like turbulence, timing, and the need for a guiding current. While ship locks generally cannot replace fish passes, they can sometimes be adapted during peak migration seasons to aid species like salmon or glass eels.');
 INSERT INTO ref.tr_fishway_fiw (fiw_code, fiw_description, fiw_definition)
 VALUES('D', 
 'Denil pass', 
-'TODO');
+'compact, steeply sloped fish pass featuring angled baffles that create backflows to reduce water velocity, enabling fish to ascend over moderate height differences. Its prefabricated design and efficient energy dissipation make it ideal for retrofitting existing dams with limited space, with the standard U-shaped baffle version now widely used.');
 INSERT INTO ref.tr_fishway_fiw (fiw_code, fiw_description, fiw_definition)
 VALUES('RR', 
 'Rock ramp', 
-'TODO');
+'Close-to_nature type, gently sloped rough-surfaced sill spanning the river width with a gentle slope, designed to overcome riverbed level differences; it may include stabilizing structures like weirs if they share similar sloped, loose construction.');
 INSERT INTO ref.tr_fishway_fiw (fiw_code, fiw_description, fiw_definition)
 VALUES('ER', 
 'Eel ramp', 
-'TODO');
+'Eel ladders/ramps, e.g., pipe-based systems laid through weirs and filled with baffles or brushwood to slow flow, shallow channels fitted with brush or gravel structures that help eels ascend while offering better visibility, maintenance, and protection from predators.');
 INSERT INTO ref.tr_fishway_fiw (fiw_code, fiw_description, fiw_definition)
 VALUES('LA', 
 'Lateral canal', 
-'TODO');
+'Bybass channel that uses lateral canals');
 INSERT INTO ref.tr_fishway_fiw (fiw_code, fiw_description, fiw_definition)
 VALUES('AR', 
 'Artificial river', 
-'TODO');
+'Human-made channel designed to mimic the characteristics of a natural stream, allowing fish to bypass barriers like dams. These fishways typically feature a gentle slope, varied flow conditions, resting areas, and natural substrates (like gravel and rocks, trees, bushes, etc.)');
 INSERT INTO ref.tr_fishway_fiw (fiw_code, fiw_description, fiw_definition)
 VALUES('UN', 
 'Unknown', 
-'TODO');
+'Unknown type fish passage');
 INSERT INTO ref.tr_fishway_fiw (fiw_code, fiw_description, fiw_definition)
 VALUES('S', 
 'Sluice', 
-'TODO');
+'Periodically opening sluice gates that creates a strong, directed flow that attracts and allows fish to pass. ');
 INSERT INTO ref.tr_fishway_fiw (fiw_code, fiw_description, fiw_definition)
-VALUES('L', 
-'Fish lift', 
-'TODO');
+VALUES('HL', 
+'Hydraulic lift', 
+'A type of fish elevator that uses hydraulic mechanisms—such as pumps, valves, and water pressure—to move fish over high barriers like dams.');
 
 
 ALTER TABLE ref.tr_fishway_fiw OWNER TO diaspara_admin;
--- SQL/metric_05_ref_tr_monitoring_mon.sql
+++ SQL/metric_05_ref_tr_monitoring_mon.sql
@@ -12,7 +12,7 @@ DELETE FROM ref.tr_monitoring_mon;
 INSERT INTO ref.tr_monitoring_mon (mon_code, mon_description, mon_definition)
 VALUES('SO', 
 'Sonar HF', 
-'High frequency sonar used to monitor fish migration, e.g. Didson, Aris, Blueview, Occulus ....');
+'Multibeam, high frequency sonar used to monitor fish migration, e.g. Didson, Aris, Blueview, Occulus ....');
 INSERT INTO ref.tr_monitoring_mon (mon_code, mon_description, mon_definition)
 VALUES('TR', 
 'Trap', 
@@ -40,6 +40,27 @@ INSERT INTO ref.tr_monitoring_mon (mon_code, mon_description, mon_definition)
 VALUES('IR', 
 'Infrared counter', 
 'Counter that uses infrared, e.g., Vaki Riverwatcher');
+INSERT INTO ref.tr_monitoring_mon (mon_code, mon_description, mon_definition)
+VALUES('OC', 
+'Other catching methods', 
+'Any other type of fish catching method than a trap. E.g. rod, gillnet, etc.');
+INSERT INTO ref.tr_monitoring_mon (mon_code, mon_description, mon_definition)
+VALUES('VC', 
+'Visual count', 
+'Visual fish counting methods, not including with the use of cameras');
+INSERT INTO ref.tr_monitoring_mon (mon_code, mon_description, mon_definition)
+VALUES('IQ', 
+'Interview or questionnaire', 
+'Interview or questionnaire of e.g. recreational fishers or other parties');
+INSERT INTO ref.tr_monitoring_mon (mon_code, mon_description, mon_definition)
+VALUES('MK', 
+'Mark and Recapture', 
+'Mark and recapture methods');
+INSERT INTO ref.tr_monitoring_mon (mon_code, mon_description, mon_definition)
+VALUES('CD', 
+'Catch data', 
+'Recreational or commercial fisheries catch data');
+
 
 
 
