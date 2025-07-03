50cbccbf4b7b58c83d22e9626ede14e576629ea0
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2025-07-03 14:48:33 +0200

    dev grouptrait

---
+++ SQL/metric_08_ref_tr_traitvaluequal_trv.sql
@@ -0,0 +1,47 @@
+-- DROP TABLE IF EXISTS ref.tr_traitvaluequal_trv CASCADE;
+
+CREATE TABLE ref.tr_traitvaluequal_trv(
+  trv_id INTEGER,
+  trv_trq_code TEXT NOT NULL,
+  CONSTRAINT fk_trv_trq_code 
+  FOREIGN KEY (trv_trq_code)
+  REFERENCES ref.tr_traitqualitative_trq(tra_code)
+  ON UPDATE CASCADE ON DELETE CASCADE,
+  trv_code text NOT NULL UNIQUE,
+  trv_description text NULL,
+  trv_spe_code TEXT NOT NULL,
+  CONSTRAINT fk_trv_spe_code  FOREIGN KEY (trv_spe_code)
+  REFERENCES ref.tr_species_spe(spe_code)
+  ON UPDATE CASCADE ON DELETE RESTRICT,
+  trv_wkg_code TEXT NOT NULL,
+  CONSTRAINT fk_trv_wkg_code  FOREIGN KEY (trv_wkg_code)
+  REFERENCES ref.tr_icworkinggroup_wkg(wkg_code)
+  ON UPDATE CASCADE ON DELETE RESTRICT,  
+  CONSTRAINT uk_trv_code UNIQUE (trv_code, trv_trq_code,trv_wkg_code)
+);
+
+
+COMMENT ON COLUMN ref.tr_traitvaluequal_trv.trv_id IS 'Integer, id of the qualitative used';
+COMMENT ON COLUMN ref.tr_traitvaluequal_trv.trv_code IS 'Code of the qualitative trait';
+COMMENT ON COLUMN ref.tr_traitvaluequal_trv.trv_description IS 'Description of the method';
+
+GRANT ALL ON ref.tr_traitvaluequal_trv TO diaspara_admin;
+GRANT SELECT ON ref.tr_traitvaluequal_trv TO diaspara_read; 
+
+DROP TABLE IF EXISTS refeel.tr_traitvaluequal_trv;
+CREATE TABLE refeel.tr_traitvaluequal_trv(
+  CONSTRAINT uk_trv_id UNIQUE (trv_id),
+  CONSTRAINT fk_trv_trq_code 
+  FOREIGN KEY (trv_trq_code)
+  REFERENCES refeel.tr_traitqualitative_trq(tra_code)
+  ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT uk_refeel_trv_code UNIQUE (trv_code, trv_trq_code)
+) INHERITS (ref.tr_traitvaluequal_trv);
+
+
+COMMENT ON COLUMN refeel.tr_traitvaluequal_trv.trv_id IS 'Integer, id of the qualitative used';
+COMMENT ON COLUMN refeel.tr_traitvaluequal_trv.trv_code IS 'Code of the qualitative trait';
+COMMENT ON COLUMN refeel.tr_traitvaluequal_trv.trv_description IS 'Description of the method';
+
+GRANT ALL ON refeel.tr_traitvaluequal_trv TO diaspara_admin;
+GRANT SELECT ON refeel.tr_traitvaluequal_trv TO diaspara_read; 
--- SQL/metric_26_dat_t_grouptrait_grt.sql
+++ SQL/metric_26_dat_t_grouptrait_grt.sql
@@ -1,5 +1,5 @@
 
--- DROP TABLE datawg.t_grouptrait_grt;
+-- DROP TABLE IF EXISTS dat.t_grouptrait_grt;
 
 
 CREATE TABLE dat.t_grouptrait_grt (
@@ -17,37 +17,86 @@ CREATE TABLE dat.t_grouptrait_grt (
   ON UPDATE CASCADE ON DELETE RESTRICT,
   grt_id serial4 NOT NULL,
   grt_gr_id int4 NOT NULL,
-  CONSTRAINT fk_grt_gr_id FOREIGN KEY (grt_gr_id) 
-  REFERENCES dat.t_group_gr(gr_id) 
-  ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT fk_grt_gr_id FOREIGN KEY (grt_gr_id, grt_wkg_code) 
+  REFERENCES dat.t_group_gr(gr_id,gr_wkg_code) 
+    ON UPDATE CASCADE ON DELETE CASCADE,
   grt_tra_code TEXT NOT NULL,
   CONSTRAINT fk_grt_tra_code FOREIGN KEY (grt_tra_code) 
-  REFERENCES refeel.tr_trait_tra(tra_code)
+  REFERENCES ref.tr_trait_tra(tra_code)
   ON UPDATE CASCADE ON DELETE RESTRICT,
   grt_value numeric NOT NULL,
-  grt_qal_code TEXT,
-  CONSTRAINT fk_grt_qal_code FOREIGN KEY (grt_qal_code)
-  REFERENCES refeel.tr_traitqualitative_trq(qal_code)
+  grt_trv_code TEXT,
+  CONSTRAINT fk_grt_qal_code FOREIGN KEY (grt_trv_code)
+  REFERENCES ref.tr_traitvaluequal_trv(trv_code)
   ON UPDATE CASCADE ON DELETE RESTRICT,
-  grt_trm_code TEXT, 
+  grt_trm_code TEXT,
+  CONSTRAINT fk_grt_trm_code FOREIGN KEY (grt_trm_code)
+  REFERENCES ref.tr_traitmethod_trm(trm_code)
+  ON UPDATE CASCADE ON DELETE RESTRICT,
   grt_last_update date DEFAULT CURRENT_DATE NOT NULL,
-  grt_qal_id int4 NULL,
+  grt_qal_code int4 NULL,
+    CONSTRAINT fk_grt_qal_id FOREIGN KEY (grt_qal_code) 
+  REFERENCES ref.tr_quality_qal(qal_code) ON UPDATE CASCADE,
   grt_ver_code TEXT NOT NULL,
   CONSTRAINT fk_grt_ver_code FOREIGN KEY (grt_ver_code)
   REFERENCES ref.tr_version_ver(ver_code)
   ON UPDATE CASCADE ON DELETE RESTRICT,
-  CONSTRAINT ck_uk_grt_gr UNIQUE (grt_gr_id, grt_mty_id, grt_wkg_code),
-  CONSTRAINT t_grouptrait_grt_pkey PRIMARY KEY (grt_id, grt_wkg_code),
-
-
-
-  CONSTRAINT fk_grt_qal_id FOREIGN KEY (grt_qal_id) 
-  REFERENCES "ref".tr_quality_qal(qal_id) ON UPDATE CASCADE
+  CONSTRAINT ck_uk_grt_gr UNIQUE (grt_gr_id, grt_trm_code, grt_wkg_code),
+  CONSTRAINT t_grouptrait_grt_pkey PRIMARY KEY (grt_id, grt_wkg_code)
 );
-CREATE INDEX t_grt_group_gr_idx ON dat.t_grouptrait_meg USING btree (grt_gr_id);
+CREATE INDEX t_grt_group_gr_idx ON dat.t_grouptrait_grt USING btree (grt_gr_id);
 
-GRANT ALL ON dat.t_grouptrait_meg TO diaspara_admin;
-GRANT SELECT ON dat.t_grouptrait_meg TO diaspara_read; 
+GRANT ALL ON dat.t_grouptrait_grt TO diaspara_admin;
+GRANT SELECT ON dat.t_grouptrait_grt TO diaspara_read; 
+
+
+
+CREATE TABLE dateel.t_grouptrait_grt (
+ CONSTRAINT fk_grt_ser_id FOREIGN KEY (grt_ser_id)
+    REFERENCES dat.t_series_ser (ser_id) 
+    ON UPDATE CASCADE ON DELETE CASCADE,  
+  grt_wkg_code TEXT NOT NULL,  
+  CONSTRAINT fk_grt_wkg_code  FOREIGN KEY (grt_wkg_code)
+  REFERENCES ref.tr_icworkinggroup_wkg(wkg_code)
+  ON UPDATE CASCADE ON DELETE RESTRICT,
+  grt_spe_code TEXT NOT NULL,  
+  CONSTRAINT fk_grt_spe_code  FOREIGN KEY (grt_spe_code)
+  REFERENCES ref.tr_species_spe(spe_code)
+  ON UPDATE CASCADE ON DELETE RESTRICT,
+  grt_id serial4 NOT NULL,
+  grt_gr_id int4 NOT NULL,
+  CONSTRAINT fk_grt_gr_id FOREIGN KEY (grt_gr_id, grt_wkg_code) 
+  REFERENCES dat.t_group_gr(gr_id,gr_wkg_code) 
+    ON UPDATE CASCADE ON DELETE CASCADE,
+  grt_tra_code TEXT NOT NULL,
+  CONSTRAINT fk_grt_tra_code FOREIGN KEY (grt_tra_code) 
+  REFERENCES ref.tr_trait_tra(tra_code)
+  ON UPDATE CASCADE ON DELETE RESTRICT,
+  grt_value numeric NOT NULL,
+  grt_trv_code TEXT,
+  CONSTRAINT fk_grt_qal_code FOREIGN KEY (grt_trv_code)
+  REFERENCES ref.tr_traitvaluequal_trv(trv_code)
+  ON UPDATE CASCADE ON DELETE RESTRICT,
+  grt_trm_code TEXT,
+  CONSTRAINT fk_grt_trm_code FOREIGN KEY (grt_trm_code)
+  REFERENCES ref.tr_traitmethod_trm(trm_code)
+  ON UPDATE CASCADE ON DELETE RESTRICT,
+  grt_last_update date DEFAULT CURRENT_DATE NOT NULL,
+  grt_qal_code int4 NULL,
+    CONSTRAINT fk_grt_qal_id FOREIGN KEY (grt_qal_code) 
+  REFERENCES ref.tr_quality_qal(qal_code) ON UPDATE CASCADE,
+  grt_ver_code TEXT NOT NULL,
+  CONSTRAINT fk_grt_ver_code FOREIGN KEY (grt_ver_code)
+  REFERENCES ref.tr_version_ver(ver_code)
+  ON UPDATE CASCADE ON DELETE RESTRICT,
+  CONSTRAINT ck_uk_grt_gr UNIQUE (grt_gr_id, grt_trm_code, grt_wkg_code),
+  CONSTRAINT t_grouptrait_grt_pkey PRIMARY KEY (grt_id, grt_wkg_code)
+) INHERITS (dat.t_grouptrait_grt);
+CREATE INDEX t_grt_group_gr_idx ON dat.t_grouptrait_grt USING btree (grt_gr_id);
+
+GRANT ALL ON dat.t_grouptrait_grt TO diaspara_admin;
+GRANT SELECT ON dat.t_grouptrait_grt TO diaspara_read; 
+
 
 
 
