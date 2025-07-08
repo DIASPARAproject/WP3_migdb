f4fa2c56220644aab77d6702749b720a655b36ed
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2025-07-08 18:04:33 +0200

    dev : switching to server

---
+++ bash/exchange_with_server.ps1
@@ -0,0 +1,15 @@
+# Dump referientials to server 28/04/25
+pg_dump --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara --table ref.tr_trait_tra -v | psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara
+
+pg_dump --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara --table 'ref."EDMO"' -v | psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara
+
+pg_dump --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara --table 'ref."EDMO"' -v | psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara
+
+pg_dump --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara --table 'ref."PRGOV"' --table 'ref."PURPM"' --table 'ref."SemanticRelation"' --table 'ref."Station_DTYPE"' -v | psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara
+
+pg_dump --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara --table 'ref."WLTYP"' --table 'ref."MSTAT"' --table 'ref."Deprecated"' --table 'ref."StationDictionary"' -v | psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara
+
+pg_dump --dbname=postgresql://${env:userlocal}:${env:passlocal}@${env:hostdiaspara}/diaspara --table ref.tr_fishway_fiw --table ref.tr_gear_gea --table ref.tr_habitattype_hty --table ref.tr_monitoring_mon --table ref.tr_sex_sex --table ref.tr_traitmethod_trm --table ref.tr_traitnumeric_trn --table ref.tr_traitqualitative_trq --table ref.tr_traitvaluequal_trv  --table ref.tr_habitatlevel_hab -v | psql --dbname=postgresql://${env:usermercure}:${env:passmercure}@${env:hostmercure}/diaspara
+
+
+TODO change foreign key constraint on 
