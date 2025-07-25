03d35b38ce1dae5c0b8cbb621155267b9ce8ac72
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2025-07-25 17:07:04 +0200

    dev : fix foreign data wrapper

--- SQL/0_foreign_data_wrapper.sql
+++ SQL/0_foreign_data_wrapper.sql
@@ -12,7 +12,7 @@ CREATE SERVER wgnas_data_wrapper
 
 CREATE USER MAPPING FOR USER
   SERVER wgeel_data_wrapper
-  OPTIONS (user 'postgres', password 'postgres');
+  OPTIONS (user 'postgres', password 'postgres', updatable 'false');
   
 CREATE USER MAPPING FOR USER
   SERVER wgnas_data_wrapper
@@ -49,5 +49,14 @@ SELECT * FROM pg_stat_get_activity(NULL::integer)
 ALTER FOREIGN TABLE datwgeel.t_eelstock_eel OPTIONS (ADD updatable 'false');
 ALTER FOREIGN TABLE datwgeel.t_series_ser OPTIONS (ADD updatable 'false');
 ALTER FOREIGN TABLE datwgeel.t_fish_fi OPTIONS (ADD updatable 'false');
-
-
+ALTER FOREIGN TABLE datwgeel.t_group_gr OPTIONS (ADD updatable 'false');
+ALTER FOREIGN TABLE datwgeel.t_groupsamp_grsa OPTIONS (ADD updatable 'false');
+ALTER FOREIGN TABLE datwgeel.t_groupseries_grser OPTIONS (ADD updatable 'false');
+ALTER FOREIGN TABLE datwgeel.t_metricgroup_meg OPTIONS (ADD updatable 'false');
+ALTER FOREIGN TABLE datwgeel.t_metricgroupsamp_megsa OPTIONS (ADD updatable 'false');
+ALTER FOREIGN TABLE datwgeel.t_metricgroupseries_megser OPTIONS (ADD updatable 'false');
+ALTER FOREIGN TABLE datwgeel.t_metricind_mei OPTIONS (ADD updatable 'false');
+ALTER FOREIGN TABLE datwgeel.t_metricindsamp_meisa OPTIONS (ADD updatable 'false');
+ALTER FOREIGN TABLE datwgeel.t_metricindseries_meiser OPTIONS (ADD updatable 'false');
+ALTER FOREIGN TABLE datwgeel.t_seriesglm_sgl OPTIONS (ADD updatable 'false');
+ALTER FOREIGN TABLE datwgeel.t_samplinginfo_sai OPTIONS (ADD updatable 'false');
