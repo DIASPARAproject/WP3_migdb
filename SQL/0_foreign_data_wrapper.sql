b03e2131a1cf614b7754eccf48551e28d4b61f1e
olivierojules <oliviero.jules@gmail.com>
2025-08-13 09:23:08 +0200

    dev: creating data wrapper to get lagune and complex #20 #23

--- SQL/0_foreign_data_wrapper.sql
+++ SQL/0_foreign_data_wrapper.sql
@@ -6,10 +6,15 @@ CREATE SERVER wgeel_data_wrapper
   FOREIGN DATA WRAPPER postgres_fdw
   OPTIONS (host 'localhost', port '5432', dbname 'wgeel');
 
+--DROP SERVER wgnas_data_wrapper CASCADE;
 CREATE SERVER wgnas_data_wrapper
   FOREIGN DATA WRAPPER postgres_fdw
   OPTIONS (host 'localhost', port '5432', dbname 'salmoglob');
 
+CREATE SERVER eda_data_wrapper
+  FOREIGN DATA WRAPPER postgres_fdw
+  OPTIONS (host 'localhost', port '5432', dbname 'eda2.3');
+ 
 CREATE USER MAPPING FOR USER
   SERVER wgeel_data_wrapper
   OPTIONS (user 'postgres', password 'postgres');
@@ -18,7 +23,11 @@ CREATE USER MAPPING FOR USER
   SERVER wgnas_data_wrapper
   OPTIONS (user 'postgres', password 'postgres');
 
+CREATE USER MAPPING FOR USER
+  SERVER eda_data_wrapper
+  OPTIONS (user 'postgres', password 'postgres');
 
+ 
 CREATE SCHEMA refwgeel;
 IMPORT FOREIGN SCHEMA ref    
     FROM SERVER wgeel_data_wrapper
@@ -42,5 +51,13 @@ IMPORT FOREIGN SCHEMA public
     INTO refsalmoglob;
   
 GRANT ALL PRIVILEGES ON SCHEMA refsalmoglob TO diaspara_admin;
+GRANT USAGE ON SCHEMA refsalmoglob TO diaspara_read;
 
-SELECT * FROM pg_stat_get_activity(NULL::integer) 
+CREATE SCHEMA dateda;
+IMPORT FOREIGN SCHEMA france    
+	LIMIT TO (tr_lagunes, tr_zoneifremer_zif)
+    FROM SERVER eda_data_wrapper
+    INTO dateda;
+  
+  
+GRANT ALL PRIVILEGES ON SCHEMA dateda TO diaspara_admin;
